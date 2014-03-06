7755f89f65d7fb20ac0d37e640029c530ac124eb
Sven Schiffer <svenschiffer92@gmail.com>
2014-03-06 09:40:37 +0100

    Implemented search fallback

--- application.php
+++ application.php
@@ -28,11 +28,13 @@ class Application {
 
 	function get($query = null) {
 		$query = strtolower($query);
+		$count = 0;
 
 		echo '<?xml version="1.0"?>';
 		echo '<items>';
 		foreach ($this->emoticons as $key => $value) {
 			if(preg_match('/^'.$query.'/', $key)) {
+				$count++;
 				echo '
 				<item uid="'.$key.'" arg="'.$value.'">
 					<title>'.$value.'</title>
@@ -40,6 +42,17 @@ class Application {
 					<icon>icon.png</icon>
 				</item>';
 			}
+		}
+		/* 
+		* Fallback 
+		*/
+		if($count == 0) {
+			echo '
+				<item uid="empty" arg="">
+					<title>Nothing found for "'.$query.'".</title>
+					<subtitle>Try other searchwords. Try e.g. "'.array_rand($this->emoticons).'"</subtitle>
+					<icon>icon.png</icon>
+				</item>';
 		}
 		echo '</items>';
 	}
